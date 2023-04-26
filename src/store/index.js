f256ee33865b75f86daaccd338f6c4839d53d9e9
0rigina1 <378787677@qq.com>
2023-04-26 22:02:43 +0800

    配置Vuex-along维持状态管理数据持久化

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexAlong from 'vuex-along'
+
 
 Vue.use(Vuex)
 
@@ -7,20 +9,21 @@ export default new Vuex.Store({
     state: {
         auth: null
     },
-    getters: {
-        isAuthenticated(state) {
-            return state.auth !== null
-        }
-    },
+    getters: {},
     mutations: {
         setAuth(state, val) {
             state.auth = val
         },
     },
     actions: {
-        setAuth({ commit }, val) {
+        setAuth({commit}, val) {
             commit('setAuth', val)
         },
     },
-    modules: {}
+    modules: {},
+    plugins: [VuexAlong({
+        name: 'store',
+        session: {list: [], isFilter: true},
+        justSession: true
+    })]
 })
